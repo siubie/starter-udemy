7cde2208f147cd94bd5e169afcb8a19ba51333d1
Putra Prima Arhandi <putraprima@gmail.com>
2022-11-27 08:58:46 +0700

    Install Fortify

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,22 @@ Route::get('/', function () {
     return view('dashboard.home');
 });
 
-Route::get('/baru', function () {
-    return view('dashboard.baru');
-});
+// Route::get('/baru', function () {
+//     return view('dashboard.baru');
+// });
 
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
+// Route::get('/login', function () {
+//     return view('auth.login');
+// })->name('login');
 
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
+// Route::get('/register', function () {
+//     return view('auth.register');
+// })->name('register');
 
-Route::get('/forgot', function () {
-    return view('auth.forgot');
-})->name('forgot');
+// Route::get('/forgot', function () {
+//     return view('auth.forgot');
+// })->name('forgot');
 
-Route::get('/reset', function () {
-    return view('auth.reset');
-})->name('reset');
+// Route::get('/reset', function () {
+//     return view('auth.reset');
+// })->name('reset');
