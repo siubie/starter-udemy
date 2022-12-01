f72811651b38b5bf4f36a30ecdfcb47a3f2b46f7
Putra Prima Arhandi <putraprima@gmail.com>
2022-12-02 06:06:57 +0700

    Fixing Database Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::factory(10)->create();
+
+        User::create([
+            'name' => 'Test User',
+            'email' => 'soko@soko.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
