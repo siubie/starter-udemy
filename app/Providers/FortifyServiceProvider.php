cdbf31eae503f55606119d22966b27bf663d7496
Putra Prima Arhandi <putraprima@gmail.com>
2022-12-02 05:33:08 +0700

    Contoh Login View

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -39,11 +39,15 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
 
-            return Limit::perMinute(5)->by($email.$request->ip());
+            return Limit::perMinute(5)->by($email . $request->ip());
         });
 
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
         });
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.custom')
+@section('title', 'Login')
 @section('content')
     <div class="row">
         <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
@@ -12,30 +13,39 @@
                 </div>
 
                 <div class="card-body">
-                    <form method="POST" action="#" class="needs-validation" novalidate="">
+                    <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+                        @csrf
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input id="email" type="email" class="form-control" name="email" tabindex="1" required
-                                autofocus>
-                            <div class="invalid-feedback">
-                                Please fill in your email
-                            </div>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                name="email" tabindex="1" autofocus>
+                            @error('email')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
 
                         <div class="form-group">
                             <div class="d-block">
                                 <label for="password" class="control-label">Password</label>
                                 <div class="float-right">
-                                    <a href="{{ route('forgot') }}" class="text-small">
+                                    <a href="" class="text-small">
                                         Forgot Password?
                                     </a>
                                 </div>
                             </div>
-                            <input id="password" type="password" class="form-control" name="password" tabindex="2"
-                                required>
-                            <div class="invalid-feedback">
-                                please fill in your password
-                            </div>
+                            <input id="password" type="password"
+                                class="form-control
+                                @error('password')
+                                is-invalid
+                            @enderror"
+                                name="password" tabindex="2">
+                            @error('password')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
                         <div class="form-group">
                             <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
