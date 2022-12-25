f315f4da07cd4f69fe22915d8df77615b5003e0b
Putra Prima Arhandi <putraprima@gmail.com>
2022-12-26 05:41:25 +0700

    tambah user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,10 +20,18 @@ class UserSeeder extends Seeder
         User::factory(10)->create();
 
         User::create([
-            'name' => 'Test User',
+            'name' => 'Super Admin',
             'email' => 'superadmin@gmail.com',
             'email_verified_at' => now(),
             'role' => 'superadmin',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'role' => 'admin',
             'password' => Hash::make('password'),
         ]);
     }
